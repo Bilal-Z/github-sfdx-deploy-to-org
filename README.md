afe9e77a833126b0ef06e7fdba6d89c47146f43f
Bilal Z <bilalzubairi@hotmail.com>
2023-01-23 19:31:51 +0500

    added initial implementaion

---
+++ README.md
@@ -0,0 +1,5 @@
+# GitHub-SFDX-Deploy-to-Org
+
+deploys source to salesforce.
+
+based on: https://github.com/tiagonnascimento/sfdx-orgdev-build-deploy
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/sh -l
 
+# https://linuxhint.com/set-command-bash/
+set -e
+set -u
+
+LOGIN_URL="${1}"
+CERT_PATH="${2}"
+DECRYPT_KEY="${3}"
+DECRYPT_IV="${4}"
+CONSUMER_KEY="${5}"
+USERNAME="${6}"
+TEST_LEVEL="${7}"
+TESTS_TO_RUN="${8}"
+DEPLOY_WAIT="${9}"
+SRC_PATH="${10}"
+
 # https://developer.salesforce.com/blogs/2022/01/set-up-continuous-integration-for-your-salesforce-projects
 
-
+echo ":: Install sfdx cli"
 # Download the Salesforce CLI installer
 wget https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz
 
@@ -19,4 +34,12 @@ echo "$HOME/sfdx/bin" >> $GITHUB_PATH
 # and keep a trace of the version in the logs for debugging purposes
 ~/sfdx/bin/sfdx version
 
-~/sfdx/bin/sfdx force:source:manifest:create --sourcepath force-app --manifestname manifest/cicd-package
+echo ":: Decrypt Certificate"
+
+openssl enc -nosalt -aes-256-cbc -d -in "$CERT_PATH" -out server.key -base64 -K "$DECRYPT_KEY" -iv "$DECRYPT_IV"
+
+echo ":: Authenticating into org"
+~/sfdx/bin/sfdx force:auth:jwt:grant --instanceurl "$LOGIN_URL" --clientid "$CONSUMER_KEY" --jwtkeyfile server.key --username "$USERNAME" --setalias sfdc
+
+echo ":: Deploy to org"
+~/sfdx/bin/sfdx force:source:deploy --wait "$DEPLOY_WAIT" --sourcepath "$SRC_PATH" --testlevel "$TEST_LEVEL" --targetusername sfdc
